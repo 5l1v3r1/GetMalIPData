00cef6dfd02d78c998fbe49e396613c6951295d7
Douglas Berdeaux <weaknetlabs@gmail.com>
2018-03-15 10:16:42 -0400

    Updated with [ WARNING ] msg
    Updated with final warning for any flags found.

--- getmalipdata.sh
+++ getmalipdata.sh
@@ -3,6 +3,7 @@
 # Scripted by Douglas Berdeaux 2018
 #
 IFS=$'\n'
+token=0
 printf "\e[1m\e[32mGetMalIPData - 2018 WeakNet Labs\e[0m\n\n"
 if [ "$1" != "" ]
  then
@@ -10,10 +11,23 @@ if [ "$1" != "" ]
    grep -i 'webiron.com' |\
    sed -re 's/<i class="fa fa-minus-circle text-danger" aria-hidden="true"><\/i>/\n[ FAILED ]:/g'|\
    sed -re 's/<i class="fa fa-check-circle text-success" aria-hidden="true"><\/i>/\n[ OK ]:/g'|\
-   sed -re 's/<.+href="/ /'|sed -re 's/http.*//g')
+   sed -re 's/<.+href="/ /'|\
+   sed -re 's/http.*//g'|\
+   sed -re 's/".*//g'|\
+   sed -re 's/<.*//g')
   do
+   # update our counter / tracker for bad sites:
+   if [[ $status =~ .*FAILED.* ]]
+    then
+    let "token++"
+   fi
    printf "$status\n";
   done;
+  # let user know if site flagged:
+  if [ $token -gt 0 ]
+   then
+    printf "\n[ WARNING ] IP $1 has been flagged by \e[4mat least one\e[24m source!\n\n";
+  fi
 else
  printf "Usage: ./getmalurldata.sh <ip>\n";
  exit 1;
